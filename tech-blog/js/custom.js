d0e644ee751a57ae5681e33170cd6f64ea3d8917
José Robson <joserobsonsiqueira23@hotmail.com>
2024-04-20 10:47:13 -0300

    fix: bugs

--- tech-blog/js/custom.js
+++ tech-blog/js/custom.js
@@ -1,9 +1,3 @@
-/**************************************
-    File Name: custom.js
-    Template Name: Tech Blog
-    Created By: HTML.Design
-    http://themeforest.net/user/wpdestek
-**************************************/
 
 (function($) {
     "use strict";
